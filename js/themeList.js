3e17bab3680b139fad8bc45a76b21cec2ae29173
JaRoet <jeroenophetweb@gmail.com>
2026-09-13 15:59:02 +0200

    removed unused and leftover files and references.

--- js/themeList.js
+++
@@ -1,7 +0,0 @@
-(function(J) {
-    // Configuratie van beschikbare thema's
-    J.Themes = [
-        { id: 'light', name: 'JaRoet Light (Default)', file: '' }, // Leeg = default variabelen uit style.css
-        { id: 'dark', name: 'JaRoet Dark', file: 'css/themes/dark.css' }
-    ];
-})(window.Jaroet);
